53eb0f0235e818363330b73580c41a9269bce545
Pervez <pervezhossain1998@gmail.com>
2022-11-07 20:48:47 +0600

    Middleware and DotEnv Require Done

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 
+// MiddleWare
+
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+require("dotenv").config()
+
 
 
 
