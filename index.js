574d4c3e13b8f3512593d5e6cf616159dc6c2fc2
Pervez <pervezhossain1998@gmail.com>
2022-11-07 22:20:07 +0600

    Database Connection Stablished

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // MiddleWare
 
 const cors = require("cors")
@@ -10,6 +12,22 @@ app.use(cors())
 app.use(express.json())
 require("dotenv").config()
 
+// Database Setup
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xu0oole.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        
+    }
+    finally {
+        
+    }
+}
+run().catch(err=> console.log(err))
 
 
 
