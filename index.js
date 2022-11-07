6b9b0060f0140ac63170f79921bc149e2366cb0c
Pervez <pervezhossain1998@gmail.com>
2022-11-07 23:39:01 +0600

    dynamic api crated for all service detail

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // MiddleWare
 
-const cors = require("cors")
+const cors = require("cors");
+const { ObjectId } = require("bson");
 
 app.use(cors())
 app.use(express.json())
@@ -23,10 +24,29 @@ async function run() {
     try {
         const serviceCollection = client.db("travlesCare").collection("services")
 
+        // For 3 Services
         app.get("/services", async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+
+        // For All Services
+        app.get("/allservices", async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // Service Detail Api With Dynamic Id
+
+        app.get("/allservices/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
+            
             res.send(result)
         })
     }
