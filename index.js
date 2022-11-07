8ec91514cad235f2437e86eca23b48ebb978f8c5
Pervez <pervezhossain1998@gmail.com>
2022-11-07 22:27:18 +0600

    service api created

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        
+        const serviceCollection = client.db("travlesCare").collection("services")
+
+        app.get("/services", async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
     }
     finally {
         
