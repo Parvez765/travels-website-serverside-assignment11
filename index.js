0483fc2db5fbfe6a94d76db4aa728a30813489f3
Pervez <pervezhossain1998@gmail.com>
2022-11-08 12:54:10 +0600

    Review Delete Api Created

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
             console.log(result)
             res.send(result)
         })
+
+        app.delete("/myreview/:id", async(req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally {
         
